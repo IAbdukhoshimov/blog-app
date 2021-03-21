4f582e126debcea250305f05e17643a9b9569341
Madiyor <m.abdukhashimov@gmail.com>
2021-03-21 16:19:16 +0500

    added the user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { v4: uuidv4 } = require("uuid");
+const logger = require("../config/logger");
+const bycrypt = require("bcrypt");
+const SALT_FACTOR = 10;
 
 let UserSchema = new Schema({
     id: {
@@ -24,5 +27,41 @@ let UserSchema = new Schema({
         required: true
     }
 });
+
+UserSchema.pre("save", async function (next) {
+    let user = this;
+
+    if (user.isModified("password")) {
+        return next();
+    }
+
+    try {
+        let salt = await bycrypt.genSalt(SALT_FACTOR);
+        user.password = await bycrypt.hash(user.password, salt);
+    } catch (error) {
+        logger.error("Error while hashing the user password", {
+            error: error
+        });
+
+        throw new Error(error.message);
+    }
+
+    return next();
+});
+
+UserSchema.methods.comparePassword = async function (insertedPassword) {
+    let isMatch = false;
+
+    try {
+        isMatch = await bycrypt.compare(insertedPassword, this.password);
+        return isMatch;
+    } catch (error) {
+        logger.error("Error while hashing the user password", {
+            error: error
+        });
+
+        throw new Error(error.message);
+    }
+};
 
 module.exports = mongoose.model("User", UserSchema);
