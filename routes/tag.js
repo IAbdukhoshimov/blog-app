3c38822118a8a89b2410cccf0cf6869a1c5dec9a
Madiyor <m.abdukhashimov@gmail.com>
2021-03-22 19:34:36 +0500

    added storage calls

--- routes/tag.js
+++ routes/tag.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../config/logger");
+const tagStorage = require("../storage/mongodb/tags");
 
 router.post("/", async (req, res) => {
     logger.info("POST tags request");
 
     try {
+        const response = await tagStorage.createTag(req.body);
+        return res.status(201).send({ id: response });
     } catch (error) {
         logger.error("Error while creating tag", {
             function: "router.tag.post",
@@ -20,6 +23,8 @@ router.get("/", async (req, res) => {
     logger.info("GET tags request");
 
     try {
+        const response = await tagStorage.getAllTags();
+        return res.status(200).send(response);
     } catch (error) {
         logger.error("Error while retreiving all tag", {
             function: "router.tag.get",
@@ -34,6 +39,8 @@ router.get("/:id", async (req, res) => {
     logger.info("GET tags request");
 
     try {
+        const response = await tagStorage.getTag(req.params.id);
+        return res.send(response);
     } catch (error) {
         logger.error("Error while retreiving tag", {
             function: "router.tag.get",
@@ -48,6 +55,8 @@ router.put("/:id", async (req, res) => {
     logger.info("PUT tags request");
 
     try {
+        const response = await tagStorage.updateTag(req.params.id, req.body);
+        return res.send({ id: response });
     } catch (error) {
         logger.error("Error while updating tag", {
             function: "router.tag.put",
@@ -62,6 +71,8 @@ router.delete("/:id", async (req, res) => {
     logger.info("DELETE tags request");
 
     try {
+        await tagStorage.deleteTag(req.params.id);
+        return res.status(200).send({ msg: "Deleted successfully" });
     } catch (error) {
         logger.error("Error while deleting tag", {
             function: "router.tag.delete",
--- storage/mongodb/tags.js
+++ storage/mongodb/tags.js
@@ -74,3 +74,5 @@ let tagStorage = {
         }
     }
 };
+
+module.exports = tagStorage;
