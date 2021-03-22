1dc8fe766b5baeb2f994ea787400cdc303f98b8a
Madiyor <m.abdukhashimov@gmail.com>
2021-03-22 21:22:16 +0500

    Added get post

---
+++ storage/mongodb/post.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Post = require("../../models/post");
+const { PostValidator } = require("../../validators/post");
+
+let postStorage = {
+    create: async (data) => {
+        const { value, error } = PostValidator.validate(data);
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        try {
+            const post = new Post(value);
+            const res = await post.save();
+            return res.id;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+    get: async (id) => {
+        if (!id) {
+            throw new Error("id is required to get post");
+        }
+
+        try {
+            const res = await Post.findOne({ id: id }).populate({
+                path: "category",
+                model: "Category"
+            });
+            return res;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+    getAll: async () => {},
+    update: async (id, data) => {},
+    delete: async (id) => {}
+};
