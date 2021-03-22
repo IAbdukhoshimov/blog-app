f9e1bdc277124541c4096dd46ff6bc22c150335d
Madiyor <m.abdukhashimov@gmail.com>
2021-03-22 19:18:03 +0500

    Added category routes

---
+++ routes/category.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const logger = require("../config/logger");
+
+router.post("/", async (req, res) => {
+    logger.info("GET tags request");
+
+    try {
+    } catch (error) {
+        logger.error("Error while xyz tag", {
+            function: "router.tag.get",
+            error: error
+        });
+
+        res.status(500).send({ error: error.message });
+    }
+});
+
+router.get("/", async (req, res) => {
+    logger.info("GET tags request");
+
+    try {
+    } catch (error) {
+        logger.error("Error while xyz tag", {
+            function: "router.tag.get",
+            error: error
+        });
+
+        res.status(500).send({ error: error.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    logger.info("GET tags request");
+
+    try {
+    } catch (error) {
+        logger.error("Error while xyz tag", {
+            function: "router.tag.get",
+            error: error
+        });
+
+        res.status(500).send({ error: error.message });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    logger.info("GET tags request");
+
+    try {
+    } catch (error) {
+        logger.error("Error while xyz tag", {
+            function: "router.tag.get",
+            error: error
+        });
+
+        res.status(500).send({ error: error.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    logger.info("GET tags request");
+
+    try {
+    } catch (error) {
+        logger.error("Error while xyz tag", {
+            function: "router.tag.get",
+            error: error
+        });
+
+        res.status(500).send({ error: error.message });
+    }
+});
